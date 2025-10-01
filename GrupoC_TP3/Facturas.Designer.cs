4cc7d1bf60c6380837df05c159c9545cfb821d3f
jero.sere <jero.sere@gmail.com>
2025-10-01 20:27:12 -0300

    Form. Facturas v0

---
+++ GrupoC_TP3/Facturas.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GrupoC_TP3
+{
+    partial class Facturas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nro. de Cliente: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(143, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(154, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(29, 26);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(428, 133);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Cliente";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(29, 193);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(428, 227);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Cuenta Corriente";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(317, 39);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Facturas
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Facturas";
+            Text = "Facturas";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button1;
+    }
+}

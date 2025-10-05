7bf943751ac0a9010b94505697a5c7ec8842db28
alan mogliani <alanbrianmogliani@hotmail.com>
2025-10-04 21:35:17 -0300

    ABM: carga de pantalla para imposicion en CD.

---
+++ GrupoC_TP3/CU9_RegistrarImposicionEnCD/RegistrarImposicionEnCDForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoC_TP3.CU9_RegistrarImposicionEnCD
+{
+    public partial class RegistrarImposicionEnCDForm : Form
+    {
+        public RegistrarImposicionEnCDForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

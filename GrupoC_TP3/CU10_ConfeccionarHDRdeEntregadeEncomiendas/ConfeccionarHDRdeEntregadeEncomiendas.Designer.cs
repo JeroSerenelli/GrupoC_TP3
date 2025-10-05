145894e9863a0eb855ac05dc57de9837eba2bae7
ritte <ritte@AGUSTIN>
2025-10-05 10:56:49 -0300

    Confeccionar HDR de ENTREGA V1

---
+++ GrupoC_TP3/CU10_ConfeccionarHDRdeEntregadeEncomiendas/ConfeccionarHDRdeEntregadeEncomiendas.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace GrupoC_TP3.CU10_ConfeccionarHDRdeEntregadeEncomiendas
+{
+    partial class ConfeccionarHDRdeEntregadeEncomiendas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            comboBox2 = new ComboBox();
+            listView1 = new ListView();
+            NumerodeEncomiendasHDREntrega = new ColumnHeader();
+            ClienteHDREntrega = new ColumnHeader();
+            OrigenHDREntrega = new ColumnHeader();
+            DestinoHDREntrega = new ColumnHeader();
+            TelefonoHDREntrega = new ColumnHeader();
+            CantidaddePaquetesHDREntrega = new ColumnHeader();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(listView1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(39, 30);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(647, 360);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Confeccionar Hoja de Ruta de Entrega";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Metodo de entrega";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Entrega en domicilio", "Entrega en CD destino", "Entrega en Agencia" });
+            comboBox1.Location = new Point(202, 52);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(65, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Estado";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(202, 93);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { NumerodeEncomiendasHDREntrega, ClienteHDREntrega, OrigenHDREntrega, DestinoHDREntrega, TelefonoHDREntrega, CantidaddePaquetesHDREntrega });
+            listView1.GridLines = true;
+            listView1.Location = new Point(6, 151);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(604, 97);
+            listView1.TabIndex = 4;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // NumerodeEncomiendasHDREntrega
+            // 
+            NumerodeEncomiendasHDREntrega.Text = "Numero de encomienda";
+            NumerodeEncomiendasHDREntrega.Width = 100;
+            // 
+            // ClienteHDREntrega
+            // 
+            ClienteHDREntrega.Text = "Cliente";
+            ClienteHDREntrega.Width = 100;
+            // 
+            // OrigenHDREntrega
+            // 
+            OrigenHDREntrega.Text = "Origen";
+            OrigenHDREntrega.Width = 100;
+            // 
+            // DestinoHDREntrega
+            // 
+            DestinoHDREntrega.Text = "Destino";
+            DestinoHDREntrega.Width = 100;
+            // 
+            // TelefonoHDREntrega
+            // 
+            TelefonoHDREntrega.Text = "Telefono";
+            TelefonoHDREntrega.Width = 100;
+            // 
+            // CantidaddePaquetesHDREntrega
+            // 
+            CantidaddePaquetesHDREntrega.Text = "N° de paquetes";
+            CantidaddePaquetesHDREntrega.Width = 100;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(417, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(54, 293);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 34);
+            button2.TabIndex = 6;
+            button2.Text = "Generar Hoja de Ruta";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(365, 299);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 28);
+            button3.TabIndex = 7;
+            button3.Text = "Imprimir";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // ConfeccionarHDRdeEntregadeEncomiendas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "ConfeccionarHDRdeEntregadeEncomiendas";
+            Text = "ConfeccionarHDRdeEntregadeEncomiendas";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private ComboBox comboBox2;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private ListView listView1;
+        private ColumnHeader NumerodeEncomiendasHDREntrega;
+        private ColumnHeader ClienteHDREntrega;
+        private ColumnHeader OrigenHDREntrega;
+        private ColumnHeader DestinoHDREntrega;
+        private ColumnHeader TelefonoHDREntrega;
+        private ColumnHeader CantidaddePaquetesHDREntrega;
+    }
+}
--- GrupoC_TP3/CU13_RegistrarAdmisiondeEncomiendasEnCD/Registrar admision de encomiendas en CD.Designer.cs
+++ GrupoC_TP3/CU13_RegistrarAdmisiondeEncomiendasEnCD/Registrar admision de encomiendas en CD.Designer.cs
@@ -29,20 +29,171 @@
         private void InitializeComponent()
         {
             folderBrowserDialog1 = new FolderBrowserDialog();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(41, 90);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(328, 397);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos del Solicitante";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Location = new Point(397, 90);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(347, 397);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Datos de la Encomienda";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(352, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 2;
+            label1.Text = "TUTASA";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(261, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(249, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Transportes Urbanos Terrestres Argentinos S.A";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Nombre del Solicitante";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 128);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Apellido del Solicitante";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 175);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Razon Social";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 217);
+            label6.Name = "label6";
+            label6.Size = new Size(53, 15);
+            label6.TabIndex = 3;
+            label6.Text = "Telefono";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(16, 50);
+            label7.Name = "label7";
+            label7.Size = new Size(102, 15);
+            label7.TabIndex = 0;
+            label7.Text = "Cantidad de Cajas";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(16, 76);
+            label8.Name = "label8";
+            label8.Size = new Size(76, 15);
+            label8.TabIndex = 1;
+            label8.Text = "Tipo de cajas";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(16, 111);
+            label9.Name = "label9";
+            label9.Size = new Size(163, 15);
+            label9.TabIndex = 2;
+            label9.Text = "Centro Distribucion de origen";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(16, 138);
+            label10.Name = "label10";
+            label10.Size = new Size(185, 15);
+            label10.TabIndex = 3;
+            label10.Text = "Centro de Distribucion de Destino";
             // 
             // RegistrarEncomiendas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(832, 606);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Name = "RegistrarEncomiendas";
-            Text = "Registrar Encomiendas";
+            Text = "Registrar Admision de Encomiendas";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private FolderBrowserDialog folderBrowserDialog1;
         private TextBox textBox;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private Label label2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
     }
 }

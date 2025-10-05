39b92b45123fb346ab62e211d26716f14d1c3ba7
ritte <ritte@AGUSTIN>
2025-10-05 12:49:12 -0300

    Agregado de Registracion de Admision de encomiendas

--- GrupoC_TP3/CU13_RegistrarAdmisiondeEncomiendasEnCD/Registrar admision de encomiendas en CD.Designer.cs
+++ GrupoC_TP3/CU13_RegistrarAdmisiondeEncomiendasEnCD/Registrar admision de encomiendas en CD.Designer.cs
@@ -29,52 +29,33 @@
         private void InitializeComponent()
         {
             folderBrowserDialog1 = new FolderBrowserDialog();
-            groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
             label1 = new Label();
             label2 = new Label();
+            groupBox1 = new GroupBox();
             label3 = new Label();
+            textBox1 = new TextBox();
+            listView1 = new ListView();
+            NumeroEncomiendasAdmision = new ColumnHeader();
+            ClienteAdmision = new ColumnHeader();
+            TamañoAdmision = new ColumnHeader();
+            PesoAdmision = new ColumnHeader();
+            CantidadPaqueteAdmision = new ColumnHeader();
+            EstadoAdmision = new ColumnHeader();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
             label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
-            label9 = new Label();
-            label10 = new Label();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
             // 
-            // groupBox1
-            // 
-            groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(label3);
-            groupBox1.Location = new Point(41, 90);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(328, 397);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Datos del Solicitante";
-            // 
-            // groupBox2
-            // 
-            groupBox2.Controls.Add(label10);
-            groupBox2.Controls.Add(label9);
-            groupBox2.Controls.Add(label8);
-            groupBox2.Controls.Add(label7);
-            groupBox2.Location = new Point(397, 90);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(347, 397);
-            groupBox2.TabIndex = 1;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "Datos de la Encomienda";
-            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(352, 19);
+            label1.Location = new Point(326, 18);
             label1.Name = "label1";
             label1.Size = new Size(50, 15);
             label1.TabIndex = 2;
@@ -83,93 +64,157 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(261, 50);
+            label2.Location = new Point(235, 49);
             label2.Name = "label2";
             label2.Size = new Size(249, 15);
             label2.TabIndex = 3;
             label2.Text = "Transportes Urbanos Terrestres Argentinos S.A";
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(32, 84);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(686, 87);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(6, 76);
+            label3.Location = new Point(26, 39);
             label3.Name = "label3";
-            label3.Size = new Size(128, 15);
+            label3.Size = new Size(148, 15);
             label3.TabIndex = 0;
-            label3.Text = "Nombre del Solicitante";
+            label3.Text = "Numero de la encomienda";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(223, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(188, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { NumeroEncomiendasAdmision, ClienteAdmision, TamañoAdmision, PesoAdmision, CantidadPaqueteAdmision, EstadoAdmision });
+            listView1.GridLines = true;
+            listView1.Location = new Point(6, 32);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(660, 100);
+            listView1.TabIndex = 2;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // NumeroEncomiendasAdmision
+            // 
+            NumeroEncomiendasAdmision.Text = "Numero de Encomiendas";
+            NumeroEncomiendasAdmision.Width = 120;
+            // 
+            // ClienteAdmision
+            // 
+            ClienteAdmision.Text = "Cliente";
+            ClienteAdmision.Width = 120;
+            // 
+            // TamañoAdmision
+            // 
+            TamañoAdmision.Text = "Tamaño";
+            TamañoAdmision.Width = 100;
+            // 
+            // PesoAdmision
+            // 
+            PesoAdmision.Text = "Peso";
+            PesoAdmision.Width = 100;
+            // 
+            // CantidadPaqueteAdmision
+            // 
+            CantidadPaqueteAdmision.Text = "Cantidad de Paquetes";
+            CantidadPaqueteAdmision.Width = 120;
+            // 
+            // EstadoAdmision
+            // 
+            EstadoAdmision.Text = "Estado";
+            EstadoAdmision.Width = 100;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(591, 39);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(listView1);
+            groupBox2.Location = new Point(32, 204);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(686, 203);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(6, 128);
+            label4.Location = new Point(52, 164);
             label4.Name = "label4";
-            label4.Size = new Size(128, 15);
-            label4.TabIndex = 1;
-            label4.Text = "Apellido del Solicitante";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Validar";
             // 
-            // label5
+            // comboBox1
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(6, 175);
-            label5.Name = "label5";
-            label5.Size = new Size(73, 15);
-            label5.TabIndex = 2;
-            label5.Text = "Razon Social";
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Aprobar", "Modificar" });
+            comboBox1.Location = new Point(123, 161);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 4;
             // 
-            // label6
+            // button2
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(6, 217);
-            label6.Name = "label6";
-            label6.Size = new Size(53, 15);
-            label6.TabIndex = 3;
-            label6.Text = "Telefono";
+            button2.Location = new Point(574, 160);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Modificar";
+            button2.UseVisualStyleBackColor = true;
             // 
-            // label7
+            // button3
             // 
-            label7.AutoSize = true;
-            label7.Location = new Point(16, 50);
-            label7.Name = "label7";
-            label7.Size = new Size(102, 15);
-            label7.TabIndex = 0;
-            label7.Text = "Cantidad de Cajas";
+            button3.Location = new Point(523, 438);
+            button3.Name = "button3";
+            button3.Size = new Size(91, 36);
+            button3.TabIndex = 6;
+            button3.Text = "Confirmar";
+            button3.UseVisualStyleBackColor = true;
             // 
-            // label8
+            // button4
             // 
-            label8.AutoSize = true;
-            label8.Location = new Point(16, 76);
-            label8.Name = "label8";
-            label8.Size = new Size(76, 15);
-            label8.TabIndex = 1;
-            label8.Text = "Tipo de cajas";
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(16, 111);
-            label9.Name = "label9";
-            label9.Size = new Size(163, 15);
-            label9.TabIndex = 2;
-            label9.Text = "Centro Distribucion de origen";
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Location = new Point(16, 138);
-            label10.Name = "label10";
-            label10.Size = new Size(185, 15);
-            label10.TabIndex = 3;
-            label10.Text = "Centro de Distribucion de Destino";
+            button4.Location = new Point(634, 438);
+            button4.Name = "button4";
+            button4.Size = new Size(84, 36);
+            button4.TabIndex = 7;
+            button4.Text = "Cancelar";
+            button4.UseVisualStyleBackColor = true;
             // 
             // RegistrarEncomiendas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(832, 606);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            ClientSize = new Size(730, 609);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "RegistrarEncomiendas";
             Text = "Registrar Admision de Encomiendas";
             groupBox1.ResumeLayout(false);
@@ -183,17 +228,24 @@
         #endregion
         private FolderBrowserDialog folderBrowserDialog1;
         private TextBox textBox;
-        private GroupBox groupBox1;
-        private GroupBox groupBox2;
         private Label label1;
         private Label label2;
-        private Label label6;
-        private Label label5;
-        private Label label4;
+        private GroupBox groupBox1;
         private Label label3;
-        private Label label10;
-        private Label label9;
-        private Label label8;
-        private Label label7;
+        private Button button1;
+        private TextBox textBox1;
+        private ListView listView1;
+        private ColumnHeader NumeroEncomiendasAdmision;
+        private ColumnHeader ClienteAdmision;
+        private ColumnHeader TamañoAdmision;
+        private ColumnHeader PesoAdmision;
+        private ColumnHeader CantidadPaqueteAdmision;
+        private ColumnHeader EstadoAdmision;
+        private GroupBox groupBox2;
+        private Button button2;
+        private ComboBox comboBox1;
+        private Label label4;
+        private Button button3;
+        private Button button4;
     }
 }

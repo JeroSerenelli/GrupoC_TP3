0887b93e278393299d704cf02a8a15d84fb808bf
alan mogliani <alanbrianmogliani@hotmail.com>
2025-10-06 23:56:05 -0300

    ABM: cambios adicionales CU6 - entrega de encomiendas

--- GrupoC_TP3/CU6_EntregaEncomiendas/EntregaEncomiendas.Designer.cs
+++ GrupoC_TP3/CU6_EntregaEncomiendas/EntregaEncomiendas.Designer.cs
@@ -38,6 +38,10 @@
             groupBox2 = new GroupBox();
             label17 = new Label();
             label18 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
@@ -76,9 +80,9 @@
             listViewEncomiendasARetirarAgencia.Columns.AddRange(new ColumnHeader[] { Numero_Guia });
             listViewEncomiendasARetirarAgencia.FullRowSelect = true;
             listViewEncomiendasARetirarAgencia.GridLines = true;
-            listViewEncomiendasARetirarAgencia.Location = new Point(4, 41);
+            listViewEncomiendasARetirarAgencia.Location = new Point(4, 87);
             listViewEncomiendasARetirarAgencia.Name = "listViewEncomiendasARetirarAgencia";
-            listViewEncomiendasARetirarAgencia.Size = new Size(259, 97);
+            listViewEncomiendasARetirarAgencia.Size = new Size(354, 162);
             listViewEncomiendasARetirarAgencia.TabIndex = 3;
             listViewEncomiendasARetirarAgencia.UseCompatibleStateImageBehavior = false;
             listViewEncomiendasARetirarAgencia.View = View.Details;
@@ -91,7 +95,7 @@
             // 
             // buttonConfirmar
             // 
-            buttonConfirmar.Location = new Point(141, 171);
+            buttonConfirmar.Location = new Point(236, 255);
             buttonConfirmar.Name = "buttonConfirmar";
             buttonConfirmar.Size = new Size(122, 23);
             buttonConfirmar.TabIndex = 5;
@@ -112,11 +116,15 @@
             // 
             // groupBox2
             // 
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
             groupBox2.Controls.Add(buttonConfirmar);
             groupBox2.Controls.Add(listViewEncomiendasARetirarAgencia);
-            groupBox2.Location = new Point(12, 169);
+            groupBox2.Location = new Point(12, 179);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(269, 200);
+            groupBox2.Size = new Size(364, 284);
             groupBox2.TabIndex = 11;
             groupBox2.TabStop = false;
             groupBox2.Text = "Encomiendas a Retirar";
@@ -141,11 +149,45 @@
             label18.TabIndex = 67;
             label18.Text = "TUTASA";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(9, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(9, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Apellido:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(69, 22);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(164, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(69, 55);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(164, 23);
+            textBox2.TabIndex = 8;
+            // 
             // EncomiendasARetirar
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(306, 405);
+            ClientSize = new Size(396, 473);
             Controls.Add(label17);
             Controls.Add(label18);
             Controls.Add(groupBox2);
@@ -156,6 +198,7 @@
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -172,5 +215,9 @@
         private GroupBox groupBox2;
         private Label label17;
         private Label label18;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
     }
 }

6f8a6da45d21619ea00bdb0fb7d058fc98f68969
alan mogliani <alanbrianmogliani@hotmail.com>
2025-10-06 22:20:55 -0300

    ABM: modificacion en program.cs

--- GrupoC_TP3/Program.cs
+++ GrupoC_TP3/Program.cs
@@ -14,16 +14,7 @@ namespace GrupoC_TP3
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ConsultaEstadoGuiaEnCD.ConsultaEstadoGuiaEnCDForm());
-            Application.Run(new RegistrarImposicionRetiroPorDomicilio.RegistrarImposicionRetiroPorDomicilioForm());
-            Application.Run(new RegistrarImposicionEnAgencia.RegistrarImposicionEnAgenciaForm());
-            Application.Run(new ConsultaEstadoGuiaEnCD.ConsultaEstadoGuiaEnCDForm());
-            Application.Run(new ConsultaEstadoGuiaTelefonica.ConsultaEstadoGuiaTelefonicaForm());
-            Application.Run(new ResultadoCostosVsVentas.ResultadoCostosVsVentasForm());
-            Application.Run(new ConfeccionarHojaDeRuta.ConfeccionarHDRdeTransportedeMediayLargaDistancia());
-            Application.Run(new ConfeccionarHDRdeEntregadeEncomiendas());
-            Application.Run(new ConfeccionarHDRdeRetirodeEncomiendas());
-            
+            //Application.Run(new ConsultaEstadoGuiaEnCD.ConsultaEstadoGuiaEnCDForm());
         }
     }
 }

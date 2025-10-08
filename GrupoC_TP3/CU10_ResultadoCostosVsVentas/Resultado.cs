6d7736455e0e4c3b32e55ab88c32174620162be8
alan mogliani <alanbrianmogliani@hotmail.com>
2025-10-08 18:21:27 -0300

    ABM: CU10, busqueda y display de resultados en listView.

---
+++ GrupoC_TP3/CU10_ResultadoCostosVsVentas/Resultado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoC_TP3.CU10_ResultadoCostosVsVentas
+{
+    public class Resultado
+    {
+        public string EmpresaTransporte { get; set; }
+        public int Venta { get; set; }
+        public int Costo { get; set; }
+        public int ResultadoCV { get; set; } = 0;
+    }
+}

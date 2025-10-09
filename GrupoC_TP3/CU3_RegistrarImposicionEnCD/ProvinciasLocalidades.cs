50cf10aac157ff762780655dead8330ede1e8098
jero.sere <jero.sere@gmail.com>
2025-10-08 22:56:38 -0300

    CU3 - Logica para seccion informacion destino. Saqué también algunos controladores y validaicones relacionadas que se pisaban con las actualizaciones nueva.

--- GrupoC_TP3/CU3_RegistrarImposicionEnCD/ProvinciasLocalidades.cs
+++ GrupoC_TP3/CU3_RegistrarImposicionEnCD/ProvinciasLocalidades.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrupoC_TP3.CU3_RegistrarImposicionEnCD;
 
 namespace GrupoC_TP3.CU3_RegistrarImposicionEnCD
 {
     internal class ProvinciasLocalidades
     {
+        public class Ubicacion
+        {
+            public Dictionary<string, List<string>> ProvinciasYLocalidades { get; set; }
+            public Dictionary<string, string> CodigoPostalCentroDistribucion { get; set; }
+
+            public Ubicacion()
+            {
+                ProvinciasYLocalidades = new Dictionary<string, List<string>>
+                {
+                    { "Buenos Aires", new List<string> { "La Plata", "Mar del Plata", "Bahía Blanca" } },
+                    { "Córdoba", new List<string> { "Córdoba Capital", "Villa María", "Río Cuarto" } },
+                    { "Santa Fe", new List<string> { "Rosario", "Santa Fe Capital", "Rafaela" } },
+
+                };
+
+                CodigoPostalCentroDistribucion = new Dictionary<string, string>
+                {
+                    { "1900", "Centro La Plata" },
+                    { "7600", "Centro Mar del Plata" },
+                    { "8000", "Centro Bahía Blanca" },
+                    { "5000", "Centro Córdoba Capital" },
+                    { "5900", "Centro Villa María" },
+                    { "5800", "Centro Río Cuarto" },
+                    { "2000", "Centro Rosario" },
+                    { "3000", "Centro Santa Fe Capital" },
+                    { "2300", "Centro Rafaela" }
+                };
+            }
+
+            public string ObtenerCentroDistribucion(string codigoPostal)
+            {
+                if (CodigoPostalCentroDistribucion.TryGetValue(codigoPostal, out string centro))
+                {
+                    return centro;
+                }
+                else
+                {
+                    return "Centro de distribución no encontrado";
+                }
+
+            }
+
+        }
+
+
     }
 }
+

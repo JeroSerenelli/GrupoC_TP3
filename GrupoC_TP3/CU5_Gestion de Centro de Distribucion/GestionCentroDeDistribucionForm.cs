a2dbb4e853281900186ddff03997a4b761c1a28a
alan mogliani <alanbrianmogliani@hotmail.com>
2025-10-11 19:48:59 -0300

    ABM: descripcion de validaciones en CU5.

--- GrupoC_TP3/CU5_Gestion de Centro de Distribucion/GestionCentroDeDistribucionForm.cs
+++ GrupoC_TP3/CU5_Gestion de Centro de Distribucion/GestionCentroDeDistribucionForm.cs
@@ -21,5 +21,22 @@ namespace GrupoC_TP3.Gestion_de_Centro_de_Distribucion
         {
 
         }
+
+        private void buttonBuscarMicro_Click(object sender, EventArgs e)
+        {
+            //VALIDACION 1: Que el campo no esté vacío - mensaje de error "Por favor ingrese un valor en el campo "Patente" para realizar una busqueda".
+            //VALIDACION 2: Que la patente este registrada - mensaje de error "La patente ingresada no se encuentra asociada a ninguna Hoja de Ruta".
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //VALIDACION 3: Que se haya seleccionado una fila de la lista - mensaje de error "No se ha selecccionado ninguna Hoja de Ruta para ser finalizada".
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //VALIDACION 4: Que se haya seleccionado una fila de la lista - mensaje de error "No se ha selecccionado ninguna Hoja de Ruta para ser despachada".
+        }
     }
 }

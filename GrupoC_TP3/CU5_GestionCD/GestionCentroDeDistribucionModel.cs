67d6a5d4c0e7416ba274513da4cfb21f302d1e04
jero.sere <jero.sere@gmail.com>
2025-11-11 23:22:35 -0300

    CU5 - Correcion sobre cambio de estado, filtos y otros.

--- GrupoC_TP3/CU5_GestionCD/GestionCentroDeDistribucionModel.cs
+++ GrupoC_TP3/CU5_GestionCD/GestionCentroDeDistribucionModel.cs
@@ -10,32 +10,18 @@ namespace GrupoC_TP3.CU5_GestionCD
 {
     internal class GestionCentroDeDistribucionModel
     {
-
-
         public List<EncomiendasEnTransporte>? paquetesRecibidos { get; private set; }
 
-        /*= new List<EncomiendasEnTransporte>()
-    {
-        new EncomiendasEnTransporte { Patente = "ABC123", Empresa = "ViaBariloche", HojaDeRuta = "HDR001", NroGuia = 1001, Estado = "En camino al CD" },
-        new EncomiendasEnTransporte { Patente = "ABC123", Empresa = "ViaBariloche",HojaDeRuta = "HDR002", NroGuia = 1002, Estado = "En camino al CD" },
-        new EncomiendasEnTransporte { Patente = "ABC123", Empresa = "ViaBariloche", HojaDeRuta = "HDR003", NroGuia = 1003, Estado = "En camino al CD" },
-        new EncomiendasEnTransporte { Patente = "XYZ789", Empresa = "Plusmar", HojaDeRuta = "HDR004", NroGuia = 1004, Estado = "En camino al CD" }, // No se muestra si filtrás por ABC123
-    };*/
+        public List<EncomiendasEnTransporte>? paquetesParaEntregar { get; private set; } 
 
-
-        public List<EncomiendasEnTransporte>? paquetesParaEntregar { get; private set; } /*= new List<EncomiendasEnTransporte>()
-        {
-            new EncomiendasEnTransporte { Patente = "ABC123", Empresa = "El Rapido", HojaDeRuta = "HDR010", NroGuia = 2001, Estado = "En CD. Listo para despachar." },
-            new EncomiendasEnTransporte { Patente = "ABC123", Empresa = "El Rapido", HojaDeRuta = "HDR011", NroGuia = 2002, Estado = "En CD. Listo para despachar." },
-            new EncomiendasEnTransporte { Patente = "ABC123", Empresa = "El Rapido", HojaDeRuta = "HDR012", NroGuia = 2003, Estado = "En CD. Listo para despachar." },
-            new EncomiendasEnTransporte { Patente = "XYZ789", Empresa = "ViaBariloche", HojaDeRuta = "HDR013", NroGuia = 2004, Estado = "En CD. Listo para despachar." }, // No se muestra si filtrás por ABC123
-        };*/
+        public int CodigoCDActual => CentroDistribucionAlmacen.centroDistribucionActual?.CodCentroDist ?? 0;
 
         internal void ValidacionPatente(EncomiendasEnTransporte encomiendasEnTransporte)
         {
+            //Marco el codigo del CD actual
+            int CodigoCDActual = CentroDistribucionAlmacen.centroDistribucionActual.CodCentroDist;
+            MessageBox.Show($"CD Actual: {CodigoCDActual}");
 
-
-            //{ Patente = "ABC123", Empresa = "El Rapido", HojaDeRuta = "HDR010", NroGuia = 2001, Estado = "En CD. Listo para despachar." }
 
             //Busco cada campo de requerido para llenar las listas
 
@@ -56,11 +42,11 @@ namespace GrupoC_TP3.CU5_GestionCD
             }
 
             // Buscar hojas de ruta del micro con esa patente
-            var hojasDeRuta = HojaRutaMicroAlmacen.hojasRutaMicros
+            var hojasDeRutaFiltradas = HojaRutaMicroAlmacen.hojasRutaMicros
                 .Where(h => h.PatenteMicro.ToUpperInvariant() == patente)
                 .ToList();
 
-            if (!hojasDeRuta.Any())
+            if (!hojasDeRutaFiltradas.Any())
             {
                 MessageBox.Show($"No se encontraron hojas de ruta asociadas a la patente {patente}.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
@@ -70,9 +56,33 @@ namespace GrupoC_TP3.CU5_GestionCD
 
 
 
-            List<EncomiendasEnTransporte> ObtenerPaquetesPorEstado(EstadoEncomienda estado, string estadoTexto)
+            List<EncomiendasEnTransporte> ObtenerPaquetesPorDespachar(EstadoEncomienda estado, string estadoTexto)
             {
-                return hojasDeRuta
+                //EstadoEncomienda estadoCodigo = (EstadoEncomienda)4; // Convertimos el enum a int
+
+                return hojasDeRutaFiltradas
+                    .SelectMany(h => h.NumerosGuiaMicro
+                        .Select(g => GuiaAlmacen.guias
+                            .FirstOrDefault(ga => ga.NumeroGuia == g.NumeroGuia))
+                        .Where(ga => ga != null && ga.EstadoEncomienda == estado)
+                        .Select(ga => new EncomiendasEnTransporte
+                        {
+                            Patente = patente,
+                            Empresa = empresa.EmpresaOmnibus,
+                            HojaDeRuta = h.HojaRutaMicro.ToString(),
+                            NroGuia = ga.NumeroGuia,
+                            Estado = estadoTexto
+                        })
+                    )
+                    .ToList();
+            }
+
+            List<EncomiendasEnTransporte> ObtenerPaquetesParaCD(EstadoEncomienda estado, string estadoTexto)
+            {
+                //EstadoEncomienda estadoCodigo = (EstadoEncomienda)4; // Convertimos el enum a int
+
+                return hojasDeRutaFiltradas
+                    .Where(h => h.CentroDistribucionDestino == CodigoCDActual)
                     .SelectMany(h => h.NumerosGuiaMicro
                         .Select(g => GuiaAlmacen.guias
                             .FirstOrDefault(ga => ga.NumeroGuia == g.NumeroGuia))
@@ -91,15 +101,9 @@ namespace GrupoC_TP3.CU5_GestionCD
 
             // Construir las dos listas
 
-            paquetesRecibidos = ObtenerPaquetesPorEstado(
-            EstadoEncomienda.EnTransporteEntreCentroDeDistribucion,
-            "En camino al CD"
-            );
+            paquetesRecibidos = ObtenerPaquetesPorDespachar((EstadoEncomienda)4,"En transpote a CD");
 
-            paquetesParaEntregar = ObtenerPaquetesPorEstado(
-            EstadoEncomienda.EntregadoEnCentroDeDistribucion,
-            "Entregado en CD"
-            );
+            paquetesParaEntregar = ObtenerPaquetesParaCD((EstadoEncomienda)3, "Listo para descpachar en CD");
 
 
 
@@ -228,29 +232,84 @@ namespace GrupoC_TP3.CU5_GestionCD
                     continue;
                 }
 
-                int codigoPostalCDActual = centroDestino.CodPostal;
+                //TODO: Cambiar CP Actual por el que seleccionamos en el launcher,
+                // Añadir filtro para mostrar paquetes a entregar SOLO en CD actual
+                // Crear otro loop para paquetesParaEntregar --> Listo.
+                //Actualizar estado encomiendas --> Listo.
+                // Asegurarse que paquetes despachados no puedan ser vistos nuevamente en el mismo CD Y que se puedan visualizar desde otros CD. 
 
-                if (codigoPostalCDActual == guia.CodPostalDest)
+
+
+                guia.EstadoEncomienda = (EstadoEncomienda)5;
+                guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
                 {
-                    guia.EstadoEncomienda = EstadoEncomienda.RecibidoEnCentroDistribucionDestino;
-                    guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
-                    {
-                        EstadoGuiaEnum = EstadoEncomienda.RecibidoEnCentroDistribucionDestino,
-                        Fecha = DateTime.Now,
-                        Descripcion = $"Recibido en centro de distribución destino (CD {centroDestino.Nombre})."
-                    });
-                }
-                else
-                {
-                    guia.EstadoEncomienda = EstadoEncomienda.EntregadoEnCentroDeDistribucion;
-                    guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
-                    {
-                        EstadoGuiaEnum = EstadoEncomienda.EntregadoEnCentroDeDistribucion,
-                        Fecha = DateTime.Now,
-                        Descripcion = $"Recibido en centro de distribución intermedio (CD {centroDestino.Nombre})."
-                    });
-                }
+                    EstadoGuiaEnum = (EstadoEncomienda)5,
+                    Fecha = DateTime.Now,
+                    Descripcion = "Recibido En Centro Distribucion Destino"
+                    //Descripcion = $"Recibido en centro de distribución destino (CD {centroDestino.Nombre})."
+                });
+
+                
             }
+
+            foreach (var paquete in paquetesParaEntregar)
+            {
+                var guia = GuiaAlmacen.guias.SingleOrDefault(g => g.NumeroGuia == paquete.NroGuia);
+                if (guia == null) continue;//Si no se encontró la guía en el almacén, paso al siguiente paquete
+
+
+                var hojaRuta = HojaRutaMicroAlmacen.hojasRutaMicros
+                    .FirstOrDefault(h => h.HojaRutaMicro.ToString() == paquete.HojaDeRuta);
+                if (hojaRuta == null) continue; //Si no encuentro la hoja de ruta correspondiente, salto este paquete
+
+
+                var centroDestino = CentroDistribucionAlmacen.centrosDistribucion
+                    .FirstOrDefault(cd => cd.CodCentroDist == hojaRuta.CentroDistribucionDestino);
+
+                if (centroDestino == null)  //Si la hoja de ruta apunta a un centro de distribución que no existe, también lo salto
+                {
+                    MessageBox.Show($"No se encontró el centro de distribución destino (Código: {hojaRuta.CentroDistribucionDestino}) " +
+                                    $"para la hoja de ruta {hojaRuta.HojaRutaMicro}.",
+                        "Advertencia - Centro de distribución no encontrado",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    continue;
+                }
+
+                guia.EstadoEncomienda = (EstadoEncomienda)4;
+                guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
+                {
+                    EstadoGuiaEnum = (EstadoEncomienda)4,
+                    Fecha = DateTime.Now,
+                    Descripcion = "Recibido En Centro Distribucion Destino"
+                    //Descripcion = $"Recibido en centro de distribución destino (CD {centroDestino.Nombre})."
+                });
+
+                /* ESTE CONDICIONAL SIRVE PARA VALIDAR CP CD vs CP Destino de Guia y seleccionar el estado correcto en caso de multiples CDs
+                 int codigoPostalCDActual = centroDestino.CodPostal;
+
+                 if (codigoPostalCDActual == guia.CodPostalDest)
+                 {
+                     guia.EstadoEncomienda = EstadoEncomienda.RecibidoEnCentroDistribucionDestino;
+                     guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
+                     {
+                         EstadoGuiaEnum = EstadoEncomienda.RecibidoEnCentroDistribucionDestino,
+                         Fecha = DateTime.Now,
+                         Descripcion = $"Recibido en centro de distribución destino (CD {centroDestino.Nombre})."
+                     });
+                 }
+                 else
+                 {
+                     guia.EstadoEncomienda = EstadoEncomienda.EntregadoEnCentroDeDistribucion;
+                     guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
+                     {
+                         EstadoGuiaEnum = EstadoEncomienda.EntregadoEnCentroDeDistribucion,
+                         Fecha = DateTime.Now,
+                         Descripcion = $"Recibido en centro de distribución intermedio (CD {centroDestino.Nombre})."
+                     });
+                 }*/
+            }
+
+
 
 
 
@@ -261,7 +320,8 @@ namespace GrupoC_TP3.CU5_GestionCD
             //    var nroGuia = paquete.NroGuia;
             //    var guia = GuiaAlmacen.guias.Single(g => g.NumeroGuia == nroGuia);
             //    guia.EstadoEncomienda = EstadoEncomienda.EntregadoEnCentroDeDistribucion;
-            //    //TODO: verificar si el historial se graba antes o despues.
+            //    
+            //Verificar si el historial se graba antes o despues.
             //    guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
             //    {
             //        Descripcion = "En camino a domicilio o agencia.",
@@ -276,7 +336,8 @@ namespace GrupoC_TP3.CU5_GestionCD
             //    var nroGuia = paquete.NroGuia;
             //    var guia = GuiaAlmacen.guias.Single(g => g.NumeroGuia == nroGuia);
             //    guia.EstadoEncomienda = EstadoEncomienda.EnTransporteEntreCentroDeDistribucion;
-            //    //TODO: verificar si el historial se graba antes o despues.
+
+            //Verificar si el historial se graba antes o despues.    
             //    guia.HistorialEstadosGuia.Add(new HistorialEstadoGuia
             //    {
             //        Descripcion = "En transporte entre centros de distribucion",
